2fb2fbfc50ba5073cf0112e6fefa34330c08835f
duckiedot <m.malinkiewicz01@gmail.com>
2022-11-04 12:34:29 +0100

    add login funcionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import {useLocalState} from "./util/useLocalStorage";
 import {Route, Routes} from "react-router-dom";
 import AuthComponent from "./authComponents";
 import PrivateRoute from "./PrivateRoute";
@@ -10,29 +8,6 @@ function App() {
 
     const homePath = "/"
     const loginPath = "login/"
-
-    const [jwt, setJwt] = useLocalState("", "jwt");
-
-    useEffect(() => {
-        if (jwt) {
-            return;
-        }
-        const requestBody = {
-            "username": "duckie",
-            "password": "matt2001"
-        }
-
-        fetch("api/auth/login", {
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "method": "post",
-            body: JSON.stringify(requestBody)
-        }).then((response) => Promise.all([response.json(), response.headers]))
-            .then(([body, headers]) => {
-                setJwt(headers.get("authorization"));
-            })
-    }, []);
 
     return (
         <Routes>
--- frontend/src/authComponents/loginComponent.js
+++ frontend/src/authComponents/loginComponent.js
@@ -1,7 +1,27 @@
 import React, {useRef} from 'react';
 import AuthStyle from "./style"
+import {useLocalState} from "../util/useLocalStorage";
 
 const LoginComponent = () => {
+
+    const [jwt, setJwt] = useLocalState("", "jwt");
+    const loginForm = useRef(null)
+
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        const requestBody = new FormData(loginForm.current)
+        fetch("api/auth/login", {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "method": "post",
+            body: JSON.stringify(requestBody)
+        }).then((response) => Promise.all([response.json(), response.headers]))
+            .then(([body, headers]) => {
+                setJwt(headers.get("authorization"));
+            })
+    }
 
     const handleClick = () => {
         window.scrollTo({
@@ -19,10 +39,11 @@ const LoginComponent = () => {
                     <input type="text" style={AuthStyle.loginInputStyle} placeholder="Username"/>
                     <input type="password" style={AuthStyle.loginInputStyle} placeholder="Password"/>
                 </div>
-                <button style={AuthStyle.loginButtonStyle}>Log In</button>
+                <button type="submit" style={AuthStyle.loginButtonStyle}>Log In</button>
             </form>
             <h2 style={AuthStyle.registerPrompts}>don't have an account?</h2>
-            <h4 onClick={handleClick} style={AuthStyle.registerPrompts}>Click To <span style={AuthStyle.greenText}>Register Now</span></h4>
+            <h4 onClick={handleClick} style={AuthStyle.registerPrompts}>Click To <span style={AuthStyle.greenText}>Register Now</span>
+            </h4>
         </>
     );
 };
